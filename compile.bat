f3dbce3668a07f8ebeba5b207c11f550ea676ea2
Alisson Ayo <alisson.ayo15@gmail.com>
2026-07-07 03:34:43 -0500

    fix: compile.bat usa for /r con comillas para rutas con espacios

--- compile.bat
+++ compile.bat
@@ -4,7 +4,7 @@ echo ==================================
 if exist bin rmdir /s /q bin
 mkdir bin
 echo Compilando todas las clases...
-dir /s /B src\*.java > sources.txt
+(for /r src %%f in (*.java) do echo "%%f") > sources.txt
 javac -encoding UTF-8 -d bin -cp "lib\*" @sources.txt
 del sources.txt
 if %errorlevel% neq 0 (
@@ -14,7 +14,7 @@ if %errorlevel% neq 0 (
 )
 echo Copiando imagenes y recursos...
 if exist src\com\ironcladbox\images (
-    xcopy /Y /I src\com\ironcladbox\images\* bin\com\ironcladbox\images\ 2>nul
+    xcopy /Y /I "src\com\ironcladbox\images\*" "bin\com\ironcladbox\images\" 2>nul
 )
 if exist resources (
     xcopy /Y /E resources\* bin\ 2>nul
